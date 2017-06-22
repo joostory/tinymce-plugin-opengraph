a25549616e974d5043c197ba5596f05195c27680
Joo <joo.yoon@kakaocorp.com>
2017-06-22 20:11:34 +0900

    preview ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import validate from 'validate.js'
+
+const STATUS_READY = 1
+const STATUS_FETCHING = 2
+const STATUS_VIEW = 3
+const STATUS_FAILED = 4
+
+
 class App {
   constructor(editor) {
     this.editor = editor
     this.win = null
+    this.opengraph = null
+    this.status = STATUS_READY
 
+    this.fetchHandler = this.editor.settings.opengraph && this.editor.settings.opengraph.fetch_handler ? this.editor.settings.opengraph.fetch_handler : () => {}
     this.onSubmit = this.onSubmit.bind(this)
     this.onCancel = this.onCancel.bind(this)
+    this.onKeydown = this.onKeydown.bind(this)
   }
 
   open() {
-    this.win = this.editor.windowManager.open({
+    let win = this.editor.windowManager.open({
       title: '미디어 삽입',
       items: [{
         type: 'container',
@@ -24,12 +36,86 @@ class App {
         onclick: this.onCancel
       }]
     })
+
+    this.$input = win.$('.mce-opengraph-input')
+    this.$body = win.$('.mce-opengraph-body')
+    this.$input.on("keydown", this.onKeydown)
+    this.$input[0].focus()
+
+    this.win = win
+
+    this.showBody()
   }
 
   close() {
     if (this.win) {
       this.win.close()
+      this.opengraph = null
+      this.$input.off("keydown")
+      this.$input = null
+      this.$body = null
+      this.status = STATUS_READY
       this.win = null
+    }
+  }
+
+  showBody() {
+    const { $body, opengraph, status } = this
+    switch (status) {
+      case (STATUS_VIEW):
+        this.showOpengraph()
+        break;
+      case (STATUS_FAILED):
+        $body.html("<span class='mce-opengraph-error'>정보 가져오기 실패</span>")
+        break;
+      case (STATUS_FETCHING):
+        $body.html("정보 가져오는 중")
+        break;
+      default:
+        $body.html("이 곳에 미리보기가 표시됩니다")
+    }
+  }
+
+  fetchOpengraph(value) {
+    this.status = STATUS_FETCHING
+    this.showBody()
+
+    this.fetchHandler(value, (data) => {
+      if (data) {
+        this.opengraph = data
+        this.status = STATUS_VIEW
+      } else {
+        this.status = STATUS_FAILED
+      }
+      this.showBody()
+    })
+  }
+
+  showOpengraph() {
+    const { opengraph, $body } = this
+    let body;
+    if (opengraph.mediaUrl) {
+      body = `<iframe class="mce-opengraph-media" src="${opengraph.mediaUrl}" frameborder="0" allowfullscreen></iframe>`
+    } else {
+      body = ''
+      if (opengraph.image) {
+        body += `<div class="mce-opengraph-image"><img src="${opengraph.image}"></div>`
+      }
+      body += `<div class="mce-opengraph-content"><div class="mce-opengraph-title">${opengraph.title}</div><div class="mce-opengraph-description">${opengraph.description}</div><div class="mce-opengraph-host">${opengraph.host}</div></div>`
+    }
+    $body.html(`<div class="mce-opengraph-preview">${body}</div>`)
+  }
+
+  onKeydown(e) {
+    let keyCode = e.keyCode
+    let value = this.$input[0].value
+    if (keyCode === 13 || keyCode === 9) {
+      if (validate({ website: value }, { website: { url: true } })) {
+        this.status = STATUS_FAILED
+        this.showBody()
+      } else {
+        this.fetchOpengraph(value)
+      }
     }
   }
 
