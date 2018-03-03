cec86973d7e5ced0c8714aa13bc36dc597868463
Joo <joo@joostory.net>
2018-03-04 02:01:40 +0900

    실제로 fetch 하도로 수정

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const webpack = require('webpack')
 const middleware = require('webpack-dev-middleware')
 const webpackConfig = require('./webpack.config')
 const handlebars = require('express-handlebars')
+const fetcher = require('opengraph-fetcher')
 const express = require('express')
 const app = express()
 
@@ -16,6 +17,12 @@ app.use(middleware(webpack(webpackConfig), {
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/opengraph', (req, res) => {
+  fetcher.fetch(req.query.url, opengraph => {
+    res.send(opengraph)
+  })
 })
 
 app.listen(3000, () => console.log('tinymce-plugin-opengraph dev app listening on port 3000.'))
